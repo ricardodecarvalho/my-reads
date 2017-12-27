c683305a41a414c09150d20197d9928b7b4108e2
Ricardo de Carvalho <skapitao@gmail.com>
2017-12-27 10:26:09 -0200

    fix: I changed the Book class to a Functional Stateless Component

--- src/Book.js
+++ src/Book.js
@@ -1,48 +1,42 @@
-import React, {Component} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import SelectMoveTo from './SelectMoveTo'
 import Authors from './Authors'
 
-class Book extends Component {
-    static propTypes = {
-        book: PropTypes.object.isRequired,
-        books: PropTypes.array.isRequired,
-        onMoveBook: PropTypes.func.isRequired
-    }
-
-    render() {
-        const {book, books, onMoveBook} = this.props
-
-        return (
-            <li>
-                <div className="book">
-                    <div className="book-top">
-                        {book.imageLinks !== undefined && (
-                            <div className="book-cover" style={{
-                                width: 128,
-                                height: 193,
-                                backgroundImage: `url(${book.imageLinks.smallThumbnail})`
-                            }}></div>
-                        )}
-                        <div className="book-shelf-changer">
-                            <SelectMoveTo
-                                currentCategory={book.shelf}
-                                book={book}
-                                books={ books }
-                                onMoveBook={onMoveBook}
-                            />
-                        </div>
-                    </div>
-                    <div className="book-title">
-                        {book.title}
-                    </div>
-                    {book.authors !== undefined && (
-                        <Authors authors={book.authors}/>
-                    )}
+const Book = (props) => (
+    <li>
+        <div className="book">
+            <div className="book-top">
+                {props.book.imageLinks !== undefined && (
+                    <div className="book-cover" style={{
+                        width: 128,
+                        height: 193,
+                        backgroundImage: `url(${props.book.imageLinks.smallThumbnail})`
+                    }}></div>
+                )}
+                <div className="book-shelf-changer">
+                    <SelectMoveTo
+                        currentCategory={props.book.shelf}
+                        book={props.book}
+                        books={ props.books }
+                        onMoveBook={props.onMoveBook}
+                    />
                 </div>
-            </li>
-        )
-    }
+            </div>
+            <div className="book-title">
+                {props.book.title}
+            </div>
+            {props.book.authors !== undefined && (
+                <Authors authors={props.book.authors}/>
+            )}
+        </div>
+    </li>
+)
+
+Book.propTypes = {
+    book: PropTypes.object.isRequired,
+    books: PropTypes.array.isRequired,
+    onMoveBook: PropTypes.func.isRequired
 }
 
 export default Book
