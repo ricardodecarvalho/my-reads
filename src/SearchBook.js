aab924e218d041b25ccdb16061d106064d03aca9
Ricardo de Carvalho <skapitao@gmail.com>
2017-12-27 10:04:11 -0200

    fix: implemented debounce in the search field

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -1,10 +1,9 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
+import {DebounceInput} from 'react-debounce-input'
 import PropTypes from 'prop-types'
 import * as BooksAPI from './utils/BooksAPI'
 import Book from './Book'
-//import escapeRegExp from 'escape-string-regexp'
-//import sortBy from 'sort-by'
 
 class SearchBook extends Component {
     static propTypes = {
@@ -26,13 +25,15 @@ class SearchBook extends Component {
 
         let newBooks = []
 
+        console.log(query);
+
         if (query.trim() === '') {
             this.clearQuery()
             this.setState({showingBooks: newBooks})
             return
         }
 
-        this.setState({ query: query, loading: true })
+        this.setState({query: query, loading: true})
 
         BooksAPI.search(query).then(books => {
             if (!books.error) {
@@ -55,15 +56,19 @@ class SearchBook extends Component {
                 <div className="search-books-bar">
                     <Link to="/" className="close-search">Close</Link>
                     <div className="search-books-input-wrapper">
-                        <input
-                            ref={(input) => {
+
+                        <DebounceInput
+                            debounceTimeout={400}
+                            inputRef={(input) => {
                                 this.nameInput = input;
                             }}
+                            element="input"
                             type="text"
                             placeholder="Search by title or author"
                             value={query}
                             onChange={(event) => this.searchBooks(event.target.value)}
                         />
+
                     </div>
                 </div>
                 <div className="search-books-results">
